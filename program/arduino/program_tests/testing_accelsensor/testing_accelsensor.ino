9f57aad53e4ab2fde032bca6f00e554f8f7b802a
sakuyuno <forestry.for@gmail.com>
2021-09-20 18:14:39 +0900

    make new file

---
+++ program/arduino/program_tests/testing_accelsensor/testing_accelsensor.ino
@@ -0,0 +1,32 @@
+// 付属の動作確認用スケッチ
+
+#include <Wire.h>
+
+void setup()
+{
+	Wire.begin();
+	Serial.begin(9600);
+	while (!Serial);
+}
+
+bool slavePresent(byte adr)
+{
+	Wire.beginTransmission(adr);
+	return(Wire.endTransmission() == 0);
+}
+
+void loop()
+{
+	Serial.println("I2C slave device list.");
+
+	for (byte adr = 1; adr < 127; adr++) {
+
+		if (slavePresent(adr)) {
+			if (adr < 16) Serial.print("0");
+			Serial.print(adr, HEX);
+			Serial.print(" ");
+		}
+	}
+	Serial.println("\nDone.");
+	delay(5000);
+}
